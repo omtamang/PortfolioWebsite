b6472278758f145d2a2f247d87e8faff32a571d2
Om Tamanag <np03cs4s220426@heraldcollege.edu.np>
2024-10-08 09:45:43 +0545

    Added a about me page responsive

--- src/components/About/Aboutme.jsx
+++ src/components/About/Aboutme.jsx
@@ -16,25 +16,26 @@ export default function Aboutme() {
 
             
             {/* About me section */}
-            <div className="pt-[130px] h-screen flex-wrap">
+            <div className="pt-[140px] h-auto md:w-10/12 m-auto flex-wrap md:flex md:pb-[100px]">
 
-                <div className="w-full flex justify-center">
-                    <img className="w-9/12 rounded-2xl" src="/img/photo.jpg" alt="Om Tamang"/>
+                <div className=" flex justify-center">
+                    <img className="w-9/12 rounded-2xl md:w-[500px] md:h-[470px]" src="/img/photo.jpg" alt="Om Tamang"/>
                 </div>
 
-                <div className="font-poopins text-2xl text-slate-400 text-center pt-4">
-                    <p>About me</p>
-                </div>
+                <div className="text-slate-700 vsm:text-[10px] font-poopins w-10/12 md:w-5/12 m-auto text-center pb-1">
 
-                <div className="text-slate-700 vsm:text-[10px] font-poopins w-10/12 m-auto text-center">
-                    <p className=" pt-3 ">Hi, I'm Om, a passionate <b>React</b> and <b>Spring Boot</b> developer 
+                    <div className="font-poopins text-2xl text-slate-400 text-center pt-4">
+                        <p>About me</p>
+                    </div>
+
+                    <p className="pt-3 md:text-xl">Hi, I'm Om, a passionate <b>React</b> and <b>Spring Boot</b> developer 
                     with a knack for creating seamless, <b>responsive web applications</b>. 
                     With a strong foundation in <b>full-stack development</b>, I love turning complex ideas into intuitive digital experiences. 
                     My journey in tech has been driven by curiosity and the desire to solve <b>real-world problems</b>, 
                     especially those faced by communities in <b>Nepal</b>.
                     </p>
 
-                    <p className=" pt-3">
+                    <p className="pt-3 md:text-xl">
                     Outside of coding, I'm a curious traveler and enjoy uncovering the hidden stories behind famous cities. 
                     Whether I'm working on a new project or learning the latest tech trends, I'm always up for a challenge!
                     </p>
